e1dc6585f43f6e94b9fa4449a8e5929c066250c1
priori <leonardopriori@gmail.com>
2022-06-12 22:35:37 -0300

    fix useEvent

--- src/util/useEvent.ts
+++ src/util/useEvent.ts
@@ -3,12 +3,14 @@ import { useRef } from 'react';
 export function useEvent<T extends (...a: Parameters<T>) => ReturnType<T>>(
   fn: T
 ): T {
+  const fnRef = useRef(fn);
+  fnRef.current = fn;
   const ref = useRef<T | null>(null);
   if (!ref.current)
     // eslint-disable-next-line func-names
     ref.current = function (...args) {
       const self = this as unknown;
-      return fn.apply(self, args);
+      return fnRef.current.apply(self, args);
     } as T;
   return ref.current as T;
 }
