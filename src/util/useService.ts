7caf394058b8013b5c8d9e5a8a058b7334a92b4b
Leonardo Priori <leonardopriori@gmail.com>
2022-07-11 22:55:16 -0300

    Refactor useService hook

--- src/util/useService.ts
+++ src/util/useService.ts
@@ -27,48 +27,50 @@ type ServiceState<T> =
       reload: () => void;
     };
 export function useService<T>(
-  func: () => Promise<T>,
+  func0: () => Promise<T>,
   deps: DependencyList
 ): ServiceState<T> {
   const [count, setCount] = useState(0);
   const reload = useEvent(() => {
     setCount(count + 1);
   });
-  const [state, set] = useState({
+  const [state, setState] = useState({
     lastValidData: null as T | null,
     error: null as null | Error,
     status: 'starting' as 'starting' | 'reloading' | 'error' | 'success',
     reload,
   });
+  const firstStateRef = useRef(state);
+  const func = useEvent(func0);
   const promiseRef = useRef(null as null | Promise<T>);
   useEffect(() => {
     let mounted = true;
     const promise = func();
     promiseRef.current = promise;
-    if (state.status !== 'starting') {
-      set({
-        ...state,
+    if (firstStateRef.current.status !== 'starting') {
+      setState((state2) => ({
+        ...state2,
         status: 'reloading',
-      });
+      }));
     }
     promise.then(
       (res) => {
         if (promiseRef.current === promise && mounted) {
-          set({
-            ...state,
+          setState((state2) => ({
+            ...state2,
             lastValidData: res,
             error: null,
             status: 'success',
-          });
+          }));
         }
       },
       (err) => {
         if (promiseRef.current === promise && mounted) {
-          set({
-            ...state,
+          setState((state2) => ({
+            ...state2,
             error: err instanceof Error ? err : new Error(`${err}`),
             status: 'error',
-          });
+          }));
         }
       }
     );
@@ -76,7 +78,7 @@ export function useService<T>(
       mounted = false;
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [...deps, count]);
+  }, [count, func, firstStateRef, setState, ...deps]);
 
   return state as ServiceState<T>;
 }
