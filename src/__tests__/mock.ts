d5d3f4ff04f6657c6aea81bdad321c0324d5191e
Leonardo Priori <leonardopriori@gmail.com>
2022-07-14 22:43:20 -0300

    Fix test / fix mock error

--- src/__tests__/mock.ts
+++ src/__tests__/mock.ts
@@ -1,6 +1,9 @@
+import { TextEncoder } from 'util';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).openDatabase = jest.fn(() => {
   return {
     transaction: () => {},
   };
 });
+
+global.TextEncoder = TextEncoder;
