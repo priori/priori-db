7112b9829dcff0c63b4fe0cb804f2894f30e54f0
priori <leonardopriori@gmail.com>
2023-10-21 20:27:26 -0300

    Update errors reports

--- src/components/main/App.tsx
+++ src/components/main/App.tsx
@@ -1,9 +1,32 @@
 import { useAppState } from 'state/state';
-import { ConnectedApp } from './connected/ConnectedApp';
+import { useEventListener } from 'util/useEventListener';
+import { showError } from 'state/actions';
+import { grantError } from 'util/errors';
 import { Home } from './home/Home';
+import { ConnectedApp } from './connected/ConnectedApp';
 
 export function App() {
   const state = useAppState();
+  useEventListener(window, 'unhandledrejection', (e: PromiseRejectionEvent) => {
+    e.promise.catch((err) => {
+      showError(grantError(err));
+    });
+  });
+  useEventListener(window, 'error', (e: ErrorEvent) => {
+    showError(
+      e.error
+        ? grantError(e.error)
+        : new Error(
+            `${e.message}${
+              e.lineno
+                ? `\nLine: ${e.lineno} ${
+                    e.filename ? ` File: ${e.filename}` : ''
+                  }`
+                : ''
+            }`
+          )
+    );
+  });
   if (state.connected) return <ConnectedApp state={state} />;
   return <Home {...state} />;
 }
