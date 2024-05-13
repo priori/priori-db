67dc660cc45e7c72d66e7dbfac59eaebc2a8d642
priori <leonardopriori@gmail.com>
2024-05-12 21:39:43 -0300

    Update ConenctionConfiguration layout

--- src/components/main/home/ConnectionConfigurationForm.tsx
+++ src/components/main/home/ConnectionConfigurationForm.tsx
@@ -76,7 +76,8 @@ export function ConnectionConfigurationForm(props: NewConnectionProps) {
     } as ConnectionConfiguration);
   }
 
-  function onTestClick() {
+  function onTestClick(ev: React.MouseEvent<HTMLButtonElement>) {
+    if (ev.target instanceof HTMLButtonElement) ev.target.blur();
     const { database, host, port, user, password } = state;
     setTestResult('pending');
     listDatabases({
@@ -94,167 +95,182 @@ export function ConnectionConfigurationForm(props: NewConnectionProps) {
   }
 
   return (
-    <div className="new-connection" style={{ fontSize: 15 }}>
-      Host:{' '}
-      <input
-        placeholder="localhost"
-        defaultValue={connection ? connection.host : ''}
-        onChange={(e) =>
-          setState((state2) => ({
-            ...state2,
-            host: (e.target as HTMLInputElement).value,
-          }))
-        }
-      />
-      <br />
-      <div style={{ display: 'flex', width: '100%' }}>
-        <div style={{ flex: 1, paddingTop: 3 }}>
-          <i
-            className={
-              state.requireSsl ? 'fa fa-check-square-o' : 'fa fa-square-o'
-            }
-            onClick={() => {
-              setState((state2) => ({
-                ...state2,
-                requireSsl: !state2.requireSsl,
-              }));
-            }}
-            style={{
-              width: 28,
-              float: 'left',
-              fontSize: 25,
-            }}
-          />
-          <span
-            style={{ lineHeight: 1.1, fontSize: 16 }}
-            onClick={() => {
-              setState((state2) => ({
-                ...state2,
-                requireSsl: !state2.requireSsl,
-              }));
-            }}
-          >
-            Require
-            <br />
-            SSL
-          </span>{' '}
-        </div>
-        <div>
-          Port: <br />
+    <div className="new-connection">
+      <div>
+        <div className="new-connection__field">
+          Host:{' '}
           <input
-            placeholder="5432"
-            style={{ width: 50, marginBottom: 0 }}
-            defaultValue={connection ? connection.port : ''}
+            placeholder="localhost"
+            defaultValue={connection ? connection.host : ''}
             onChange={(e) =>
               setState((state2) => ({
                 ...state2,
-                port: (e.target as HTMLInputElement).value,
+                host: (e.target as HTMLInputElement).value,
               }))
             }
           />
         </div>
-      </div>
-      Database:{' '}
-      <input
-        placeholder="postgres"
-        defaultValue={connection ? connection.database : ''}
-        onChange={(e) =>
-          setState((state2) => ({
-            ...state2,
-            database: (e.target as HTMLInputElement).value,
-          }))
-        }
-      />
-      <br />
-      User:{' '}
-      <input
-        placeholder="postgres"
-        defaultValue={connection ? connection.user : ''}
-        onChange={(e) =>
-          setState((state2) => ({
-            ...state2,
-            user: (e.target as HTMLInputElement).value,
-          }))
-        }
-      />
-      <br />
-      <span>
-        Password:{' '}
-        <input
-          defaultValue={connection ? connection.password : ''}
-          onChange={(e) =>
-            setState((state2) => ({
-              ...state2,
-              password: (e.target as HTMLInputElement).value,
-            }))
-          }
-          type="password"
-        />
-      </span>
-      <br />
-      <div style={{ display: 'flex', paddingBottom: 5 }}>
-        <div style={{ marginRight: 10 }}>
-          <button type="button" onClick={onTestClick}>
-            Test...
-          </button>{' '}
-        </div>
-        {testResult === true ? (
-          <strong className="connection-test-success">
-            OK <i className="fa fa-check" />
-          </strong>
-        ) : testResult instanceof Error ? (
-          <span className="connection-test-error">
+        <div
+          className="new-connection__field"
+          style={{ display: 'flex', width: '100%' }}
+        >
+          <div
+            className="new-connection__require-ssl"
+            onClick={() => {
+              setState((state2) => ({
+                ...state2,
+                requireSsl: !state2.requireSsl,
+              }));
+            }}
+            tabIndex={0}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' || e.key === ' ') {
+                setState((state2) => ({
+                  ...state2,
+                  requireSsl: !state2.requireSsl,
+                }));
+              }
+            }}
+          >
             <i
-              className="fa fa-exclamation-triangle"
-              style={{ marginRight: 5 }}
+              className={
+                state.requireSsl ? 'fa fa-check-square-o' : 'fa fa-square-o'
+              }
+              style={{
+                width: 28,
+                float: 'left',
+                fontSize: 25,
+              }}
             />
-            {testResult.message}
-          </span>
-        ) : testResult ? (
-          <span className="connection-test-pending">
-            <i className="fa fa-circle-o-notch fa-spin" />
-          </span>
-        ) : null}
-      </div>
-      <div style={{ marginTop: '4px', marginBottom: '4px' }}>
-        <button onClick={() => saveAndConnect()} type="button">
-          <i className="fa fa-chain" /> Save &amp; Connect
-        </button>{' '}
-        {props.onCancel ? (
-          <button onClick={() => cancel()} className="cancel" type="button">
-            <i className="fa fa-rotate-left" /> Cancel
-          </button>
-        ) : null}
-      </div>
-      <button onClick={() => save()} type="button">
-        <i className="fa fa-save" /> Just Save
-      </button>{' '}
-      {removeConfirmation ? (
-        <Dialog onBlur={noClick} relativeTo="nextSibling">
-          <div style={{ lineHeight: '1.3em' }}>
-            Do you really want to remove this
-            <br />
-            connection configuration?
+            <span style={{ lineHeight: 1.1, fontSize: 16 }}>
+              Require
+              <br />
+              SSL
+            </span>{' '}
           </div>
           <div>
-            <button type="button" onClick={yesClick}>
-              Yes
-            </button>{' '}
-            <button type="button" onClick={noClick}>
-              No
-            </button>
+            Port: <br />
+            <input
+              placeholder="5432"
+              style={{ width: 70, marginBottom: 0 }}
+              defaultValue={connection ? connection.port : ''}
+              onChange={(e) =>
+                setState((state2) => ({
+                  ...state2,
+                  port: (e.target as HTMLInputElement).value,
+                }))
+              }
+            />
           </div>
-        </Dialog>
-      ) : null}
-      {props.onRemove ? (
-        <button
-          style={{ color: '#e00' }}
-          onClick={() => remove()}
-          type="button"
+        </div>
+        <div className="new-connection__field">
+          Database:{' '}
+          <input
+            placeholder="postgres"
+            defaultValue={connection ? connection.database : ''}
+            onChange={(e) =>
+              setState((state2) => ({
+                ...state2,
+                database: (e.target as HTMLInputElement).value,
+              }))
+            }
+          />
+        </div>
+        <div className="new-connection__field">
+          User:{' '}
+          <input
+            placeholder="postgres"
+            defaultValue={connection ? connection.user : ''}
+            onChange={(e) =>
+              setState((state2) => ({
+                ...state2,
+                user: (e.target as HTMLInputElement).value,
+              }))
+            }
+          />
+        </div>
+        <div className="new-connection__field">
+          Password:{' '}
+          <input
+            defaultValue={connection ? connection.password : ''}
+            onChange={(e) =>
+              setState((state2) => ({
+                ...state2,
+                password: (e.target as HTMLInputElement).value,
+              }))
+            }
+            type="password"
+          />
+        </div>
+      </div>
+      <div>
+        <br />
+        <div
+          style={{ display: 'flex', paddingBottom: 5, position: 'relative' }}
         >
-          <i className="fa fa-remove" /> Remove
-        </button>
-      ) : null}{' '}
+          <div style={{ marginRight: 10 }}>
+            <button type="button" onClick={onTestClick}>
+              Test...
+            </button>{' '}
+          </div>
+          {testResult === true ? (
+            <strong className="connection-test-success">
+              OK <i className="fa fa-check" />
+            </strong>
+          ) : testResult instanceof Error ? (
+            <span className="connection-test-error">
+              <i
+                className="fa fa-exclamation-triangle"
+                style={{ marginRight: 5 }}
+              />
+              {testResult.message}
+            </span>
+          ) : testResult ? (
+            <span className="connection-test-pending">
+              <i className="fa fa-circle-o-notch fa-spin" />
+            </span>
+          ) : null}
+        </div>
+        <div style={{ marginTop: '4px', marginBottom: '4px' }}>
+          <button onClick={() => saveAndConnect()} type="button">
+            <i className="fa fa-chain" /> Save &amp; Connect
+          </button>{' '}
+          {props.onCancel ? (
+            <button onClick={() => cancel()} type="button">
+              <i className="fa fa-rotate-left" /> Cancel
+            </button>
+          ) : null}
+        </div>
+        <button onClick={() => save()} type="button">
+          <i className="fa fa-save" /> Just Save
+        </button>{' '}
+        {removeConfirmation ? (
+          <Dialog onBlur={noClick} relativeTo="nextSibling">
+            <div style={{ lineHeight: '1.3em' }}>
+              Do you really want to remove this
+              <br />
+              connection configuration?
+            </div>
+            <div>
+              <button type="button" onClick={yesClick}>
+                Yes
+              </button>{' '}
+              <button type="button" onClick={noClick}>
+                No
+              </button>
+            </div>
+          </Dialog>
+        ) : null}
+        {props.onRemove ? (
+          <button
+            style={{ color: '#e00' }}
+            onClick={() => remove()}
+            type="button"
+          >
+            <i className="fa fa-remove" /> Remove
+          </button>
+        ) : null}{' '}
+      </div>
     </div>
   );
 }
