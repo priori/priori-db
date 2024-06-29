381cdd066f6286ec1608024c0797cc58c5dc0617
priori <leonardopriori@gmail.com>
2024-06-29 13:38:55 -0300

    Fix incremental fetching in mysql

--- src/db/mysql/selectExecution.ts
+++ src/db/mysql/selectExecution.ts
@@ -17,127 +17,138 @@ export async function selectExecution(
     conError = err2;
   });
   const con = con0.connection as unknown as PoolConnection;
-  return new Promise((resolve, reject) => {
-    try {
-      const queryExecution2 = con.query({
-        sql,
-        values: params,
-        rowsAsArray: true,
-      });
-      // besides type especification
-      // queryExecution2 can be undefined here in same cases
-      if (conError) {
-        reject(grantError(conError));
-        conError = null;
-        return;
-      }
-      if (!queryExecution2) {
-        reject(new Error('Query execution failed!'));
-        return;
-      }
-      let currentFields: QueryResultDataField[] | null = null;
-      const fields = () => {
-        if (currentFields) return currentFields;
-        // eslint-disable-next-line no-underscore-dangle
-        const dataFields = (
-          queryExecution2 as {
-            _fields?: { name: string; type: number }[][];
-          }
-        )._fields;
-        if (dataFields && dataFields.length) {
-          currentFields =
-            dataFields[dataFields.length - 1].map((f) => ({
-              name: f.name,
-              type:
-                f.type === 7 || (f.type && f.type >= 10 && f.type < 14)
-                  ? 'date'
-                  : undefined,
-            })) || [];
-          return currentFields;
+  try {
+    const queryExecution2 = con.query({
+      sql,
+      values: params,
+      rowsAsArray: true,
+    });
+    // besides type especification
+    // queryExecution2 can be undefined here in same cases
+    if (conError) {
+      throw grantError(conError);
+    }
+    if (!queryExecution2) {
+      throw new Error('Query execution failed!');
+    }
+    let currentFields: QueryResultDataField[] | null = null;
+    const fields = () => {
+      if (currentFields) return currentFields;
+      // eslint-disable-next-line no-underscore-dangle
+      const dataFields = (
+        queryExecution2 as {
+          _fields?: { name: string; type: number }[][];
         }
-        return null;
-      };
-      const data: SimpleValue[][] = [];
-      let resolved = false;
-      const stream = queryExecution2.stream();
-      const release = () => {
-        stream.destroy();
-        con.release();
-      };
+      )._fields;
+      if (dataFields && dataFields.length) {
+        currentFields =
+          dataFields[dataFields.length - 1].map((f) => ({
+            name: f.name,
+            type:
+              f.type === 7 || (f.type && f.type >= 10 && f.type < 14)
+                ? 'date'
+                : undefined,
+          })) || [];
+        return currentFields;
+      }
+      return null;
+    };
+    const data: SimpleValue[][] = [];
+    let released = false;
+    const release = () => {
+      if (released) return;
+      released = true;
+      con0.destroy();
+    };
+    const stream = queryExecution2.stream();
+    return new Promise((resolve, reject) => {
       let ended = false;
-      stream.on('data', (row: SimpleValue[]) => {
-        if (resolved) return;
-        fields();
-        data.push(row);
-        if (data.length % fetchSize === 0) {
-          resolved = true;
-          stream.pause();
-          const fetchMoreRows: () => Promise<QueryResultData> = () => {
-            if (ended) {
-              release();
-              return Promise.resolve({
-                fields: fields() || [],
-                rows: [...data],
-              });
-            }
-            return new Promise<QueryResultData>((_resolve, _reject) => {
-              let resolved2 = false;
-              stream.resume();
-              const prevSize = data.length;
-              stream.on('data', (row2: SimpleValue[]) => {
-                if (resolved2) return;
-                data.push(row2);
-                if (data.length % fetchSize === 0) {
-                  stream.pause();
-                  resolved2 = true;
-                  if (data.length < prevSize + fetchSize) release();
-                  _resolve({
-                    fields: fields() || [],
-                    rows: [...data],
-                    fetchMoreRows:
-                      data.length < prevSize + fetchSize
-                        ? undefined
-                        : fetchMoreRows,
-                    release:
-                      data.length < prevSize + fetchSize ? undefined : release,
-                  });
-                }
-              });
-              stream.on('end', () => {
-                if (resolved2) return;
-                _resolve({
+      let resolved = false;
+      try {
+        stream.on('data', (row: SimpleValue[]) => {
+          if (resolved) return;
+          fields();
+          data.push(row);
+          if (data.length % fetchSize === 0) {
+            resolved = true;
+            stream.pause();
+            const fetchMoreRows: () => Promise<QueryResultData> = () => {
+              if (ended) {
+                release();
+                return Promise.resolve({
                   fields: fields() || [],
                   rows: [...data],
                 });
-              });
+              }
+              return new Promise<QueryResultData>((_resolve, _reject) => {
+                let resolved2 = false;
+                stream.resume();
+                const prevSize = data.length;
+                stream.on('data', (row2: SimpleValue[]) => {
+                  if (resolved2) return;
+                  data.push(row2);
+                  if (data.length % fetchSize === 0) {
+                    stream.pause();
+                    resolved2 = true;
+                    if (data.length < prevSize + fetchSize) release();
+                    _resolve({
+                      fields: fields() || [],
+                      rows: [...data],
+                      fetchMoreRows:
+                        data.length < prevSize + fetchSize
+                          ? undefined
+                          : fetchMoreRows,
+                      release,
+                    });
+                  }
+                });
+                stream.on('end', () => {
+                  release();
+                  if (resolved2) return;
+                  resolved2 = true;
+                  _resolve({
+                    fields: fields() || [],
+                    rows: [...data],
+                  });
+                });
 
-              stream.on('error', (error) => {
-                _reject(grantError(error));
+                stream.on('error', (error) => {
+                  release();
+                  if (resolved2) return;
+                  resolved2 = true;
+                  _reject(grantError(error));
+                });
               });
+            };
+            resolve({
+              rows: [...data],
+              fields: fields() || [],
+              fetchMoreRows:
+                data.length < fetchSize ? undefined : fetchMoreRows,
+              release,
             });
-          };
+          }
+        });
+        stream.on('end', () => {
+          release();
+          ended = true;
+          if (resolved) return;
           resolve({
             rows: [...data],
             fields: fields() || [],
-            fetchMoreRows: data.length < fetchSize ? undefined : fetchMoreRows,
-            release,
           });
-        }
-      });
-      stream.on('end', () => {
-        ended = true;
-        if (resolved) return;
-        resolve({
-          rows: [...data],
-          fields: fields() || [],
         });
-      });
-
-      stream.on('error', (error) => {
+        stream.on('error', (error) => {
+          release();
+          reject(grantError(error));
+        });
+      } catch (error) {
+        release();
         reject(grantError(error));
-      });
-    } catch (error) {
-      reject(grantError(error));
-    }
-  });
+      }
+    });
+  } catch (error) {
+    con0.destroy();
+    throw grantError(error);
+  }
 }
