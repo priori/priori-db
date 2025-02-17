5df7a5c527e3cdca1e315d50ddc7b5dbd58afd07
priori <leonardopriori@gmail.com>
2025-02-17 02:00:09 -0300

    Enhance close confirmation dialog with overlay and improved styling

--- src/components/main/connected/CloseConfirmation.tsx
+++ src/components/main/connected/CloseConfirmation.tsx
@@ -6,35 +6,37 @@ export function CloseConfirmation({
   onDecline: () => void;
 }) {
   return (
-    <div
-      className="dialog--close-window"
-      onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
-        if (e.key === 'Escape') {
-          e.currentTarget.blur();
-        }
-      }}
-      tabIndex={0}
-      ref={(el) => {
-        if (el) el.focus();
-      }}
-      onBlur={(e) => {
-        const dialogEl = e.currentTarget;
-        setTimeout(() => {
-          if (dialogEl.contains(document.activeElement)) return;
-          onDecline();
-        }, 1);
-      }}
-    >
-      <div>
-        There is some running query or idle connection in transacion, are you
-        sure you want to close?
+    <div className="dialog--close-window--overlay">
+      <div
+        className="dialog--close-window"
+        onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
+          if (e.key === 'Escape') {
+            e.currentTarget.blur();
+          }
+        }}
+        tabIndex={0}
+        ref={(el) => {
+          if (el) el.focus();
+        }}
+        onBlur={(e) => {
+          const dialogEl = e.currentTarget;
+          setTimeout(() => {
+            if (dialogEl.contains(document.activeElement)) return;
+            onDecline();
+          }, 1);
+        }}
+      >
+        <div>
+          There is some running query or idle connection in transacion, are you
+          sure you want to close?
+        </div>
+        <button onClick={onDecline} type="button">
+          No
+        </button>{' '}
+        <button onClick={onConfirm} type="button">
+          Yes
+        </button>
       </div>
-      <button onClick={onDecline} type="button">
-        No
-      </button>{' '}
-      <button onClick={onConfirm} type="button">
-        Yes
-      </button>
     </div>
   );
 }
