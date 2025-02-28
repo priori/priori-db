f275a1fac35f36965f79723273943678dec73632
priori <leonardopriori@gmail.com>
2025-02-28 20:55:26 -0300

    Drop AppWrapper unnecessary component

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,11 +2,16 @@ import { assert } from 'util/assert';
 import { createRoot } from 'react-dom/client';
 import { ipcRenderer } from 'electron';
 import { forceClose } from 'util/useWindowCloseConfirm';
-import AppWrapper from './AppWrapper';
+import '../style/style.scss';
+import '../vendor/codemirror/codemirror.css';
+import '../vendor/font-awesome-4.6.3/css/font-awesome.min.css';
+import '../vendor/codemirror/codemirror';
+import '../vendor/codemirror/sql';
+import { App } from '../components/main/App';
 
 const container = document.getElementById('root');
 assert(container !== null);
-createRoot(container).render(<AppWrapper />);
+createRoot(container).render(<App />);
 
 window.onstorage = () => {
   document.body.className = localStorage.getItem('theme') || 'soft-gray-theme';
